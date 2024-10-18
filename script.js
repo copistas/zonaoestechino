45fac3da23ad472f9f24187cd92626ff37e6a8ca
Luo Keli <60009074+copistas@users.noreply.github.com>
2024-10-18 12:18:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ window.addEventListener('resize', function() {
     const newWidth = window.innerWidth;
     deviceTypeDiv.innerText = newWidth < 860 ? "Estás usando la versión móvil." : "Estás usando la versión de escritorio.";
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const today = new Date().getDate(); // Obtiene el día actual del mes
+    const todayCell = document.querySelector(`td[data-day="${today}"]`);
+
+    if (todayCell) {
+      todayCell.classList.add("highlight-today");
+    }
+});
+
