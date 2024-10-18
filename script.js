32627e2367dd2fc1df17916217e88365d2710bc2
Luo Keli <60009074+copistas@users.noreply.github.com>
2024-10-18 15:36:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 window.addEventListener('resize', function() {
     const newWidth = window.innerWidth;
+    const deviceTypeDiv = document.getElementById('device-type'); // Asegúrate de que esté definido
     deviceTypeDiv.innerText = newWidth < 860 ? "Estás usando la versión móvil." : "Estás usando la versión de escritorio.";
-/*});
+});
 
-document.addEventListener("DOMContentLoaded", function() {   */
+document.addEventListener("DOMContentLoaded", function() {
     const today = new Date().getDate(); // Obtiene el día actual del mes
     const todayCell = document.querySelector(`td[data-day="${today}"]`);
 
@@ -11,5 +12,6 @@ document.addEventListener("DOMContentLoaded", function() {   */
       todayCell.classList.add("highlight-today");
     }
 });
+
 
 
