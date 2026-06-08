0e50b5e62816c331e6d6b3bc2fd697ad4279d725
copistas <chaojingshi@gmail.com>
2026-06-08 16:31:56 -0300

    080626

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+data.trim().split("\n").forEach((row, index) => {
+
+    if (index === 0) return;
+
+    let esInicioBloque = row.trim().startsWith("**");
+    let esFinBloque = row.trim().endsWith("**");
+
+    console.log("Fila:", row);
+    console.log("Inicio:", esInicioBloque);
+    console.log("Fin:", esFinBloque);
+
+    ...
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     // =============================================
     // MENÚ PRINCIPAL (Versión corregida)
